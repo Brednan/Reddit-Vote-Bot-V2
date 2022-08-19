b9b932e0f9181cf5ca1908e4f5b09a9d5019acfd
brednan <brendanshauncaldwell@gmail.com>
2022-08-18 20:59:54 -0700

    Started working on the login function. So far, it will send a get request to Reddit's login page, and then it will parse it, and get the csrf token.

--- account.py
+++ account.py
@@ -10,9 +10,17 @@ class Account(Session):
         iproyal_username = environ['ip_royal_username']
         iproyal_password = environ['ip_royal_password']
 
-        self.proxies = {
-            'http': f'http://{iproyal_username}:{iproyal_password}@geo.iproyal.com:22323'
-        }
+        # self.proxies = {
+        #     'http': f'http://{iproyal_username}:{iproyal_password}@geo.iproyal.com:22323',
+        #     'https': f'http://{iproyal_username}:{iproyal_password}@geo.iproyal.com:22323'
+        # }
 
         self.username = username
         self.password = password
+
+    def login(self):
+        login_page = self.get('https://www.reddit.com/login/')
+        soup = BeautifulSoup(login_page.content, 'html.parser')
+
+        csrf_token = soup.find('input', {'name': 'csrf_token'})['value']
+        print(csrf_token)
--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from account import Account
 
 if __name__ == '__main__':
     account = Account('test', 'test')
+    account.login()
